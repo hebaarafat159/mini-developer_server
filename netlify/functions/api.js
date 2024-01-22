c66684ba22789995294dbd73c4f9938ee6fbf16e
hebaarafat159 <heba.arafat159@gmail.com>
2024-01-22 23:43:17 +0000

    modify netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,36 +2,16 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 import cors from "cors";
 import bodyParser from "body-parser";
-
-// import categoryRouter from '../../routers/category.js'
-// import productRouter from '../../routers/product.js'
-// import userRouter from '../../routers/user.js'
-// import listRouter from '../../routers/shoppingList.js'
-
-// import cors from "cors";
-
-// import mongoose, { Schema } from "mongoose";
-
-// mongoose.connect(`${process.env.DATABASE_URL}`);
+import coursesRouter from '../../routers/courses.js'
 
 const api = express();
-
-// api.use(
-//   cors({
-//     origin: "https://startling-mochi-356199.netlify.app", // Replace with your client's origin
-//     credentials: true, // Enable credentials (cookies) in CORS
-//   })
-// );
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use(cors());
 api.use(bodyParser.json());
-// api.use("/api/", router);
-// api.use("/api/categories", categoryRouter);
-// api.use("/api/products", productRouter);
-// api.use("/api/users", userRouter);
-// api.use("/api/shoppingLists",listRouter);
+api.use("/api/", router);
+api.use("/api/courses", coursesRouter);
 
 export const handler = serverless(api);
